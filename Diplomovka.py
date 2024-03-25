d84003bd18d2b0bc65a7ebe20d9cfd88e5542c2a
Zuza8651 <162828804+Zuza8651@users.noreply.github.com>
2024-03-25 20:49:19 +0100

    Záloha Diplomovka.py

--- Diplomovka.py
+++ Diplomovka.py
@@ -2,7 +2,9 @@
 import PIL
 from PIL import Image
 import numpy as np
+from PIL import Image as im
 import cv2
+
 
 def convert_jpg_to_png(input_path, output_path):
     try:
@@ -27,9 +29,6 @@ def convert_jpg_to_png(input_path, output_path):
 convert_jpg_to_png('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030.jpg', 'C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030.png')
 
 
-
-# convered jpg to .png -- JE
-
 def zosiv_obrazok(input_path, output_path):
     try:
         # Načítanie obrázka z formátu PNG
@@ -49,7 +48,6 @@ def zosiv_obrazok(input_path, output_path):
 # Príklad použitia metódy
 zosiv_obrazok('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030.png', 'C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030_zosiveny.png')
 
-# zosivenie -- JE
 
 def rozmazanie_gradient1(input_path, output_path):
     try:
@@ -85,10 +83,6 @@ def rozmazanie_segmentacia(input_path, output_path):
 
 
 rozmazanie_segmentacia('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030_zosiveny.png', 'C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030_segmentacia_rozmazany_obrazok.png')
-# rozmazanie v matlabe - mozno aj tu, ale gaus nebude
-
-
-# obrázky pridať a pridať koment ku rozmazaniu
 
 # trojrozmerné pole rxsxf
 
@@ -98,24 +92,15 @@ rozmazanie_segmentacia('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/
 img_data = PIL.Image.open('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030_gradient_rozmazany_obrazok.png')
 
 # Converting the image data into a NumPy array and storing it in 'img_arr'
-img_arr = np.array(img_data)
+img_arr = np.array(img_data, dtype= "int32")
 
 # Printing the NumPy array representation of the image
 print(img_arr)
 
 array_pixel = [[-1,-1], [0,-1],[1,-1],[-1,0], [1,0], [-1,1], [0,1], [1,1]]
-vector = []
+
 
 '''
-def make_vector(array):
-    for i in range(0, len(array)):
-        for j in range(0, len(array)):
-            for k in range(0, len(array)):
-                for l in range(0, len(array_pixel)):
-                    x = abs(array[i,j,k] - array[i + array_pixel[l,0], j + array_pixel[l,1],k])
-                    vector.append(x)
-'''
-
 def make_vector(array):
     for i in range(0, len(array)):
         for j in range(0, len(array[i])):
@@ -130,18 +115,38 @@ def make_vector(array):
 
 print('kto vie:', make_vector(img_arr))
 
-from itertools import permutations
-
-
-
-#Agregačné funkcie
 '''
-def aritmeticky_priemer(vektor):
-    sucet = sum(vektor)
-    priemer = sucet / len(vektor)
-    return priemer
+def uprav(x, y):
+    pole =[]
+    if x < 0:
+        x = abs(x)
+    if y < 0:
+        y = abs(y)
+    if x >= len(img_arr):
+        x = 2*len(img_arr) - x
+    if y >= len(img_arr[0]):
+        y = 2*len(img_arr[0]) - y
+    pole.append(x)
+    pole.append(y)
+    return pole
 
-'''
+def make_vector(i, j):
+    vector = []
+    for l in range(0, len(array_pixel)):
+        posx = i + array_pixel[l][0]
+        posy = j + array_pixel[l][1]
+        pospole = uprav(posx, posy)
+        # if (j + array_pixel[l][1]) > len(array[i]) or (j + array_pixel[l][1]) < 0 or (i + array_pixel[l][0]) < 0 or (i + array_pixel[l][0]) > len(array):
+        #    continue
+        a = abs(img_arr[i][j][0])
+        b = abs(img_arr[pospole[0]][pospole[1]][0])
+        w = abs(a-b)
+        vector.append(abs(w))
+    return vector
+
+print('dlzka:', len(img_arr))
+#print('makevector', make_vector(,481))
+
 def choquet_integral(vector, p):
     ch_int = 0.0
     sorted_vector = sorted(vector)
@@ -150,10 +155,31 @@ def choquet_integral(vector, p):
     for i in range(1, len(sorted_vector)):
         ch_int = ch_int + (sorted_vector[i] - sorted_vector[i-1]) * ((len(sorted_vector)-i)/len(vector))**p
     return ch_int
+def feature():
+    noveplatno = img_arr.copy()
+    for i in range(0, len(img_arr)):
+        for j in range(0, len(img_arr[i])):
+            vectoragg = make_vector(i,j)
+            aggvalue = choquet_integral(vectoragg, 0.9)
+            noveplatno[i][j][0] = aggvalue
+            noveplatno[i][j][1] = aggvalue
+            noveplatno[i][j][2] = aggvalue
+    #img = Image.fromarray(noveplatno, 'RGB')
+    img = im.fromarray(noveplatno.astype(np.uint8))
+
+    img.save('C:/Users/zuza8/Desktop/BSR_full/BSR/BSDS500/data/images/test/29030_choquetobr.png')
+    #img.save('29030_choquetobr.png')
+
+
+feature()
+
+
 
 choquet_integral_vysledok = choquet_integral([18,16,10], 0.9)
-print(choquet_integral_vysledok)
+print('Choquetov integral:', choquet_integral_vysledok)
 
+#choquet_integral_vysledok_obr = choquet_integral(vector, 0.9)
+#print('Choquetov integral obr:', choquet_integral_vysledok_obr)
 
 def CAO_choquet_integral(vector,p, set):
   new_vector=[0]*len(vector)
@@ -161,8 +187,7 @@ def CAO_choquet_integral(vector,p, set):
     new_vector[set[i]]=vector[set[i]]
   return choquet_integral(new_vector,p)
 
-print(CAO_choquet_integral([18,16,10],0.9,[0,1,2]))
-
+print('Choquetov integral na podmienenej mnozine:', CAO_choquet_integral([18,16,10],0.9,[0,1,2]))
 
 def aritmeticky_priemer(vektor,p):
     sucet = sum(vektor)
@@ -170,9 +195,7 @@ def aritmeticky_priemer(vektor,p):
     return priemer
 
 artm_priem=aritmeticky_priemer([18,16,10],0.9)
-print(artm_priem)
-
-
+print('Aritmeticky priemer:', artm_priem)
 
 def CAO_aritmeticky_priemer(vector,p, set):
   new_vector=[]
@@ -181,15 +204,14 @@ def CAO_aritmeticky_priemer(vector,p, set):
   return aritmeticky_priemer(new_vector,p)
 
 CAO_artm_priem=CAO_aritmeticky_priemer([18,16,10],0.9,[0,1,2])
-print(CAO_artm_priem)
-
+print('Aritmeticky priemer na podmienenej mnozine:', CAO_artm_priem)
 
 def mc_integral(vector, p, function):
     mc_int = 0.0
     auxiliary_vector = vector.copy()
     m_vector = []
     for i in range(0,len(vector)):
-        m_vector.insert(0, function(auxiliary_vector))
+        m_vector.insert(0, function(auxiliary_vector, p))
         auxiliary_vector.pop()
 
     sorted_vector = sorted(m_vector)
@@ -201,13 +223,10 @@ def mc_integral(vector, p, function):
 
 
 mc_integral_vysledok = mc_integral([18, 16, 10], 0.9, aritmeticky_priemer)
-print(mc_integral_vysledok)
-
-
+print('MC integral:', mc_integral_vysledok)
 
 def maximum(vector,p):
   return max(vector)
-
 
 def CAO_maximum(vector,p,set):
   new_vector=[]
@@ -215,13 +234,10 @@ def CAO_maximum(vector,p,set):
     new_vector.append(vector[set[i]])
   return maximum(new_vector,p)
 
-print(CAO_maximum([18,16,10],0.9,[1,2]))
-
-
+print('Maximum na podmienenej mnozine:', CAO_maximum([18,16,10],0.9,[1,2]))
 
 def minimum(vector,p):
   return min(vector)
-
 
 def CAO_minimum(vector,p,set):
   new_vector=[]
@@ -229,7 +245,7 @@ def CAO_minimum(vector,p,set):
     new_vector.append(vector[set[i]])
   return minimum(new_vector,p)
 
-print(CAO_minimum([18,16,10],0.9,[0,1]))
+print('Minimum na podmienenej mnozine:', CAO_minimum([18,16,10],0.9,[0,1]))
 
 
 def C_Ag_operator(vector, p, array):
@@ -240,10 +256,7 @@ def C_Ag_operator(vector, p, array):
     auxiliary_vector.append(func(vector,p))
   return auxiliary_vector
 
-
-print(C_Ag_operator([18,16,10],0.9,[aritmeticky_priemer,choquet_integral,minimum]))
-
-
+print('Operator zavisiaci na postupnostiach agregacnych funkci:', C_Ag_operator([18,16,10],0.9,[aritmeticky_priemer,choquet_integral,minimum]))
 
 def C_APdPid(vector, p, array, sets): # PI^\downarrow, PHI_\id=PHI_\uparrow pre vstupne x:=x^\uparrow
   c_app=0.0
@@ -257,10 +270,8 @@ def C_APdPid(vector, p, array, sets): # PI^\downarrow, PHI_\id=PHI_\uparrow pre 
         c_app = c_app + (auxiliary_vector[i])*((((len(auxiliary_vector) - i) / len(vector)) ** p)-(((len(auxiliary_vector) - i-1) / len(vector)) ** p))
   return c_app
 
-print(C_APdPid([18,16,10],0.9,[CAO_aritmeticky_priemer,CAO_choquet_integral,CAO_minimum],[[0,1],[0,1,2],[1]]))
+print('Na permutaciach zavisli Choquetov operator: C_APdPid=', C_APdPid([18,16,10],0.9,[CAO_aritmeticky_priemer,CAO_choquet_integral,CAO_minimum],[[0,1],[0,1,2],[1]]))
 
-
-# def C_APdPd(vector, p, array, sets): # PI^\downarrow, PHI_\down pre vstupne x:=x^\uparrow
 def C_APdPd(vector, p, array, sets): # PI^\downarrow, PHI_\down pre vstupne x:=x^\uparrow
     c_app = 0.0
     vector.sort(reverse=True)
@@ -278,8 +289,6 @@ print('Na permutaciach zavisli Choquetov operator: C_APdPd=',
           C_APdPd([18, 16, 10], 0.9, [CAO_aritmeticky_priemer, CAO_choquet_integral, CAO_minimum],
                    [[0, 1], [0, 1, 2], [1]]))
 
-
-# def C_APuPd(vector, p, array, sets): # PI^\uparrow, PHI_\downarrow pre vstupne x:=x^\uparrow
 def C_APuPd(vector, p, array, sets): # PI^\uparrow, PHI_\downarrow pre vstupne x:=x^\uparrow
     c_app = 0.0
     vector.sort(reverse=True)
@@ -297,9 +306,6 @@ print('Na permutaciach zavisli Choquetov operator: C_APuPd=',
           C_APuPd([18, 16, 10], 0.9, [CAO_aritmeticky_priemer, CAO_choquet_integral, CAO_minimum],
                    [[0, 1], [0, 1, 2], [1]]))
 
-
-
-# def C_APuPu(vector, p, array, sets): # PI^\uparrow, PHI_\id=PHI_\uparrow pre vstupne x:=x^\uparrow
 def C_APuPu(vector, p, array, sets): # PI^\uparrow, PHI_\id=PHI_\uparrow pre vstupne x:=x^\uparrow
     c_app = 0.0
     vector.sort()
@@ -317,28 +323,6 @@ print('Na permutaciach zavisli Choquetov operator: C_APuPu=',
           C_APuPu([18, 16, 10], 0.9, [CAO_aritmeticky_priemer, CAO_choquet_integral, CAO_minimum],
                    [[0, 1], [0, 1, 2], [1]]))
 
-
-#def a_choquet_integral():
-
-'''
-def mc_integral(vector, p, function):
-    mc_int = 0.0
-    auxiliary_vector = vector.copy()
-    m_vector = []
-    for i in range(0,len(vector)):
-        m_vector.insert(0, function(auxiliary_vector))
-        auxiliary_vector.pop()
-
-    sorted_vector = sorted(m_vector)
-    # pridať nulu na začiatok
-    sorted_vector.insert(0, 0)
-    for i in range(1, len(sorted_vector)):
-        mc_int = mc_int + (sorted_vector[i] - sorted_vector[i - 1]) * ((len(sorted_vector) - i) / len(vector)) ** p
-    return mc_int
-
-
-mc_integral_vysledok = mc_integral([18, 16, 10], 0.9, aritmeticky_priemer)
-print(mc_integral_vysledok)
 
 '''
 
@@ -366,12 +350,6 @@ vektor_vahy = [3/8, 3/8, 2/8]
 
 vysledok = mc_integral(vektor_hodnoty, 0.9, vazeny_aritmeticky_priemer, vektor_vahy)
 print(vysledok)
+'''
 
-
-
-
-#def c_ag_integral():
-
-
-#def c_perm_integral(vector, vector_perm_x, vector_perm_ag:
 
